42b91d26ad9141e1d0b6a4953b91ffc4bed2ba19
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-12-13 09:29:08 +0200

    Update `src/index.js` module
    - Create express app
    - Listen to server & connect to database using `DB` Class

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { DB } = require('./config');
+
+const app = express();
+
+// TODO: mount preMiddlewares
+
+// TODO: mount routes
+
+// TODO: mount postMiddlewares
+
+const server = app.listen(async () => {
+  const { NODE_ENV: MODE, PORT } = process.env;
+  await DB.connect();
+
+  console.log(`Connected to (${DB.getName()}) database! 🚀`);
+
+  console.log(`App is running in ${MODE} mode on port ${PORT}! 🚀`);
+});
+
+module.exports = { server };
